b5bc1cb95161b03820baa3552bf9561b00c3f2d0
BrainStackOverFlow <48990146+BrainStackOverFlow@users.noreply.github.com>
2020-11-24 22:37:31 +0200

    basic output

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,21 @@
 #include <stivale2.h>
 
+static char* vgaptr = 0xb8000;
+
+void terminal_printchar(char x, int row, int col) {
+    vgaptr[2*(row*80 + col)] = x;
+}
+
+void terminal_print(char* str, int row, int col) {
+    for (int i = 0; str[i]; i++ ) {
+        terminal_printchar(str[i], row, col);
+        row = (row + (col+1) / 80) % 25;
+        col = (col+1) % 80; 
+    }
+}
+
 void kmain(struct stivale2_struct* info) 
 {
-    char* vgaptr = 0xb8000;
-    vgaptr[0] = 'c';
+    terminal_print("Hello World!", 0, 0);
     while(1) { asm("hlt"); }
 }
